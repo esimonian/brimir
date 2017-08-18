a7244417ccc860f5c464ded06da6f0fd2ae80fb2
Elizabeth <esimonian@wrt-echopark.avtr.net>
2017-08-18 13:01:53 -0700

    set up mail

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Brimir
     # -- all .rb files in that directory are automatically loaded.
 
     # Change this to :ldap_authenticatable to use ldap
-    config.devise_authentication_strategy = :database_authenticatable
+    config.devise_authentication_strategy = :ldap_authenticatable
     config.i18n.default_locale = :en
     config.i18n.fallbacks = [:en]
   end
